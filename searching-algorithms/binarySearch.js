468df01e2a6725107938698e610ca7d45eb656be
James Su-Shum <sushumjames@gmail.com>
2020-11-29 16:22:07 -0500

    Alternate solution and time complexity calculation

--- searching-algorithms/binarySearch.js
+++ searching-algorithms/binarySearch.js
@@ -11,19 +11,22 @@
 // If the value is too large, move the right pointer down
 // If you never find the value, return -1
 
+// Time complexity: O(log n)
+// When input data doubles, just 1 extra step is added
+
 function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
 
-  while (left < right) {
+  while (left <= right) {
     let middle = Math.ceil((left + right) / 2);
 
     if (arr[middle] === val) {
       return middle;
     } else if (arr[middle] < val) {
-      left = middle;
+      left = middle + 1;
     } else {
-      right = middle;
+      right = middle - 1;
     }
   }
 
@@ -33,5 +36,22 @@ function binarySearch(arr, val) {
 // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const val = 8;
+const val = 5;
 console.log(binarySearch(arr, val));
+// console.log(binarySearch2(arr, val));
+
+// <----- Alternate solution ----->
+
+function binarySearch2(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === elem ? middle : -1;
+}
