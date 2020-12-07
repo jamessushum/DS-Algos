afc209ffdced0b3641a3c40b55b63ea9102f90f0
James Su-Shum <sushumjames@gmail.com>
2020-12-07 15:22:28 -0500

    Optimized bubble sort for nearly sorted arrays

--- bubble-sort/bubbleSort.js
+++ bubble-sort/bubbleSort.js
@@ -5,6 +5,10 @@
 // If arr[j] is greater than arr[j + 1], swap those two values
 // Return the sorted array
 
+// Time complexity:
+// In general, O(n^2)
+// If array is nearly sorted, O(n)
+
 function bubbleSort(arr) {
   // Starting i at arr.length instead of arr.length - 1 so we can iterate through the entire array on the 1st pass in the inner for-loop
   for (let i = arr.length; i > 0; i--) {
@@ -22,8 +26,8 @@ function bubbleSort(arr) {
 }
 
 const arr = [37, 45, 29, 8];
-console.log(bubbleSort(arr));
-console.log(bubbleSort2(arr));
+// console.log(bubbleSort(arr));
+// console.log(bubbleSort2(arr));
 
 // <----- Alternative solution ----->
 
@@ -43,3 +47,28 @@ function bubbleSort2(arr) {
 
   return arr;
 }
+
+// <----- Optimized solution for when array is nearly sorted ----->
+
+function bubbleSort3(arr) {
+  // Declare noSwaps variable
+  var noSwaps;
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+
+  return arr;
+}
+
+const nearlySortedArr = [8, 1, 2, 3, 4, 5, 6, 7];
+console.log(bubbleSort3(nearlySortedArr));
